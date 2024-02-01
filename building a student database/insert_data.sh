6ccdf528d09de1bbeed8e05d817492ddc1883469
VagTsop <61441231+VagTsop@users.noreply.github.com>
2024-02-01 20:52:17 +0200

    Update insert_data.sh

--- building a student database/insert_data.sh
+++ building a student database/insert_data.sh
@@ -2,74 +2,79 @@
 
 # Script to insert data from courses.csv and students.csv into students database
 
+# Define the psql command with options for connection and formatting
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+
+# Truncate tables to clear existing data
 echo $($PSQL "TRUNCATE students, majors, courses, majors_courses")
 
+# Process courses.csv
 cat courses.csv | while IFS="," read MAJOR COURSE
 do
+  # Check if the line is not the header
   if [[ $MAJOR != "major" ]]
   then
-    # get major_id
+    # Get major_id from majors table
     MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
 
-    # if not found
+    # If major not found, insert it into majors table
     if [[ -z $MAJOR_ID ]]
     then
-      # insert major
       INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
       if [[ $INSERT_MAJOR_RESULT == "INSERT 0 1" ]]
       then
         echo Inserted into majors, $MAJOR
       fi
 
-      # get new major_id
-    MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+      # Get the new major_id
+      MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
     fi
 
-    # get course_id
+    # Get course_id from courses table
     COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")   
 
-    # if not found
-  if [[ -z $COURSE_ID ]]
-  then
-  # insert course
-INSERT_COURSE_RESULT=$($PSQL "INSERT INTO courses(course) VALUES('$COURSE')")
-if [[ $INSERT_COURSE_RESULT == "INSERT 0 1" ]]
-then
-  echo "Inserted into courses, $COURSE"
-fi
-  # get new course_id
-COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
-fi
-    
-    # insert into majors_courses
-INSERT_MAJORS_COURSES_RESULT=$($PSQL "INSERT INTO majors_courses(major_id, course_id) VALUES($MAJOR_ID, $COURSE_ID)")
-if [[ $INSERT_MAJORS_COURSES_RESULT == "INSERT 0 1" ]]
-then
-  echo "Inserted into majors_courses, $MAJOR : $COURSE"
-fi
+    # If course not found, insert it into courses table
+    if [[ -z $COURSE_ID ]]
+    then
+      INSERT_COURSE_RESULT=$($PSQL "INSERT INTO courses(course) VALUES('$COURSE')")
+      if [[ $INSERT_COURSE_RESULT == "INSERT 0 1" ]]
+      then
+        echo "Inserted into courses, $COURSE"
+      fi
+
+      # Get the new course_id
+      COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
+    fi
+
+    # Insert the mapping of major_id and course_id into majors_courses table
+    INSERT_MAJORS_COURSES_RESULT=$($PSQL "INSERT INTO majors_courses(major_id, course_id) VALUES($MAJOR_ID, $COURSE_ID)")
+    if [[ $INSERT_MAJORS_COURSES_RESULT == "INSERT 0 1" ]]
+    then
+      echo "Inserted into majors_courses, $MAJOR : $COURSE"
+    fi
   fi
 done
 
+# Process students.csv
 cat students.csv | while IFS="," read FIRST LAST MAJOR GPA
 do
+  # Check if the line is not the header
   if [[ $FIRST != "first_name" ]]
   then
-    # get major_id
-MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+    # Get major_id from majors table
+    MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
 
-    # if not found
-if [[ -z $MAJOR_ID ]]
-then
-  # set to null
-MAJOR_ID=null
-fi
+    # If major not found, set MAJOR_ID to null
+    if [[ -z $MAJOR_ID ]]
+    then
+      MAJOR_ID=null
+    fi
 
-    # insert student
-INSERT_STUDENT_RESULT=$($PSQL "INSERT INTO students(first_name, last_name, major_id, gpa) VALUES('$FIRST', '$LAST', $MAJOR_ID, $GPA)")
-if [[ $INSERT_STUDENT_RESULT == "INSERT 0 1" ]]
-then
-  echo "Inserted into students, $FIRST $LAST"
-fi
+    # Insert student into students table
+    INSERT_STUDENT_RESULT=$($PSQL "INSERT INTO students(first_name, last_name, major_id, gpa) VALUES('$FIRST', '$LAST', $MAJOR_ID, $GPA)")
+    if [[ $INSERT_STUDENT_RESULT == "INSERT 0 1" ]]
+    then
+      echo "Inserted into students, $FIRST $LAST"
+    fi
   fi
 done
